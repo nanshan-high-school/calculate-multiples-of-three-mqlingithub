f66a12427105960c532614eab6c1ac5ea0c5b51d
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2018-12-15 09:15:47 +0800

    Update 25019multipules.cpp

--- 25019multipules.cpp
+++ 25019multipules.cpp
@@ -6,27 +6,34 @@ int main() {
   long int num1 = 0,num2 = 0,number;
   int meet = 0;
   char math;
-  cout << "請輸入要運算的算式:";
+
+  cout << "請輸入要運算的算式(兩個數字的運算):";
   cin >> str;
+
   for (int i = 0; str[i] != '\0'; i++) {
+
     if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
       math = str[i];
       meet = 1;
     }
+
     else if (meet == 0) {
       num1 = str[i]-'0' + (num1 * 10);
     }
+
     else {
       num2 = str[i]-'0' + (num2 * 10);
     }
   }
+
   if (math == '+') number = num1 + num2;
   if (math == '-') number = num1 - num2;
   if (math == '*') number = num1 * num2;
   if (math == '/') number = num1 / num2;
 
-     cout << number;
+  cout << number;
 
-  if ((number / 3) * 3 == number) cout << "是3的倍數。";
+  if (number == 0) cout << "不是3的倍數。";
+  else if ((number / 3) * 3 == number) cout << "是3的倍數。";
   else cout << "不是3的倍數。";
 }
